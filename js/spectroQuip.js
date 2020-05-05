6f1e3346292f32cc51865354dd903210e8d39f3c
AdamWise <wise.adam.jay@gmail.com>
2020-05-05 13:39:50 -0400

    configuration

--- js/spectroQuip.js
+++ js/spectroQuip.js
@@ -1,6 +1,13 @@
 console.log('spectroQuip C2020 Adam Wise');
 debug = 1;
 
+// What would be a neat next step?
+// maybe clarify something complicated?
+// how to make a parts list? (iterate over slots and check rules to print parts list)
+// how to enforce rules
+
+// maybe you can configure a spectrometer using pull downs?
+
 // create an object-oriented spectrometer (to start) configurator
 //  I want the eventual goal to be a GUI that allows dragging objects into slots on a spectrometer 
 // to start maybe just boxes
@@ -93,8 +100,10 @@ function Item(configObj){
         for (var i in self.slots){
             var currentItem = self.slots[i].item;
             if (currentItem){
-                currentItem.div.style('left', Number(self.div.style('left').split('px')[0]) + d3.event.dx)
-                currentItem.div.style('top', Number(self.div.style('top').split('px')[0]) + d3.event.dy)
+                var offSetX = self.slots[i].x;
+                var offSetY = self.slots[i].y;
+                currentItem.div.style('left', Number(self.div.style('left').split('px')[0]) + d3.event.dx + offSetX)
+                currentItem.div.style('top', p(self.div.style('top')) + offSetY)
             }
         
         }
@@ -176,7 +185,7 @@ function Item(configObj){
 
     self.unEquip = function(){
         if(self.equippedTo){
-            self.equippedTo.item = null;
+            self.equippedTo.item = null;    
         }
         self.equippedTo = null;
     }
@@ -187,6 +196,9 @@ function Item(configObj){
             var slotDiv = self.div.append('div');
             slotDiv.style('width', thisSlot.width).style('height', thisSlot.height)
             slotDiv.style('border', thisSlot.borderSpec);
+            slotDiv.style('position', 'absolute')
+            slotDiv.style('left', thisSlot.x)
+            slotDiv.style('top', thisSlot.y)
         }
     }
 
@@ -202,27 +214,44 @@ function Item(configObj){
 var items = [];
 
 var cam1Config = {'name' : 'camera 1',
-                 'type' : 'camera',
-                'parentDivSelector' : '#workSpace',
-                'label' : 'Camera 1',
-                'fill' : 'red'
+    'type' : 'camera',
+    'parentDivSelector' : '#workSpace',
+    'label' : 'Camera 1',
+    'fill' : 'red'
+};
 
-            };
+var cam2Config = {'name' : 'camera 1',
+    'type' : 'camera',
+    'parentDivSelector' : '#workSpace',
+    'label' : 'Camera 2',
+    'fill' : 'blue',
+    'x' : 100,
+    'y' : 100,
+};
 
 
 
-var camSlot = new Slot({'name' : 'directInput',
+var camSlot1 = new Slot({'name' : 'directInput',
                     'type':'camera',
                     'x' : 0,
-                    'y' : 0,
+                    'y' : -30,
                     'width': 30,
                     'height' : 30,
                     'borderSpec' : '1px solid red',
                    })
 
+var camSlot2 = new Slot({'name' : 'sideInput',
+    'type':'camera',
+    'x' : -30,
+    'y' : 50,
+    'width': 30,
+    'height' : 30,
+    'borderSpec' : '1px solid red',
+})
+
 var ky328Config = {'name' : 'Kymera 328i',
                      'type' : 'spectrometer',
-                     'slots' : [camSlot],
+                     'slots' : [camSlot1, camSlot2],
                      'parentDivSelector' : '#workSpace',
                      'label' : 'Kymera 328',
                      'height' : 100,
@@ -235,6 +264,7 @@ var ky328 = new Item(ky328Config);
 items.push(ky328)
 
 var cam1 = items.push(new Item(cam1Config));
+var cam2 = items.push(new Item(cam2Config));
 
 
 
